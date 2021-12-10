40f06f59bedc1b1f5855cc689309e28392d8675a
XiaomanYang <xiaomanyoung@login.khoury.neu.edu>
2021-12-09 19:26:50 -0800

    Scores added

---
+++ Client/app/src/main/java/edu/neu/madcourse/mathters/ScoreActivity.java
@@ -0,0 +1,69 @@
+package edu.neu.madcourse.mathters;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import mathters.R;
+
+public class ScoreActivity extends AppCompatActivity {
+
+    private TextView score;
+    private Button done;
+    DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReferenceFromUrl("https://littlemathters-default-rtdb.firebaseio.com/");
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_score);
+
+        score = findViewById(R.id.sa_score);
+        done = findViewById(R.id.sa_done);
+
+        String score_str = getIntent().getStringExtra("SCORE");
+        score.setText(score_str);
+        UserDetails.userscore = score_str;
+
+
+        done.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                databaseReference.child("users").addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        if(snapshot.hasChild(UserDetails.username)) {
+                            databaseReference.child("users").child(UserDetails.username).child("score").setValue(UserDetails.userscore);
+
+                        } else {
+                            Toast.makeText(ScoreActivity.this, "Scores not saved", Toast.LENGTH_LONG).show();
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+
+                Intent intent = new Intent(ScoreActivity.this,SetsActivity.class);
+                ScoreActivity.this.startActivity(intent);
+                ScoreActivity.this.finish();
+
+
+            }
+        });
+
+    }
+}
---
+++ Client/app/src/main/java/edu/neu/madcourse/mathters/UserDetails.java
@@ -0,0 +1,7 @@
+package edu.neu.madcourse.mathters;
+
+public class UserDetails {
+    static String username ="";
+    static String usernickname = "";
+    static String userscore = "";
+}
