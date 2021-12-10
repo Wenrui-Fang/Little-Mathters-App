4c563d78a5b183231b5fa7775b754bfd080a491c
Wenrui-Fang <fangwenrui1997@outlook.com>
2021-12-09 18:43:37 -0800

    fix

---
+++ math/app/src/main/java/edu/neu/madcourse/mathters/QuestionActivity.java
@@ -0,0 +1,448 @@
+package edu.neu.madcourse.mathters;
+
+import android.animation.Animator;
+import android.app.Dialog;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.media.MediaPlayer;
+import android.os.CountDownTimer;
+import android.os.Handler;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Vibrator;
+import android.util.ArrayMap;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.DecelerateInterpolator;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import static edu.neu.madcourse.mathters.SetsActivity.setsIDs;
+
+import mathters.R;
+
+public class QuestionActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView question, qCount, timer;
+    private Button option1, option2, option3, option4;
+    private List<Question> questionList;
+    private int quesNum;
+    private CountDownTimer countDown;
+    private int score;
+    private FirebaseFirestore firestore;
+    private int setNo;
+    private Dialog loadingDialog;
+    /**
+     * Sensor
+     */
+    private SensorManager sensorManager;
+    private ShakeSensorListener shakeListener;
+
+    /**
+     * Judge one shake at a time
+     */
+    private boolean isShake = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_question);
+
+        question = findViewById(R.id.question);
+        qCount = findViewById(R.id.quest_num);
+        timer = findViewById(R.id.countdown);
+
+        option1 = findViewById(R.id.option1);
+        option2 = findViewById(R.id.option2);
+        option3 = findViewById(R.id.option3);
+        option4 = findViewById(R.id.option4);
+
+        option1.setOnClickListener(this);
+        option2.setOnClickListener(this);
+        option3.setOnClickListener(this);
+        option4.setOnClickListener(this);
+
+        loadingDialog = new Dialog(QuestionActivity.this);
+        loadingDialog.setContentView(R.layout.loading_progressbar);
+        loadingDialog.setCancelable(false);
+        loadingDialog.getWindow().setBackgroundDrawableResource(R.drawable.progress_background);
+        loadingDialog.getWindow().setLayout(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        loadingDialog.show();
+
+        questionList = new ArrayList<>();
+
+        setNo = getIntent().getIntExtra("SETNO", 1);
+        firestore = FirebaseFirestore.getInstance();
+
+        getQuestionsList();
+
+        score = 0;
+
+        // Sensor Manager
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        shakeListener = new ShakeSensorListener();
+
+
+    }
+
+    private void getQuestionsList() {
+        questionList.clear();
+
+        firestore.collection("QUIZ").document(SplashActivity.catList.get(SplashActivity.selected_cat_index).getId())
+                .collection(setsIDs.get(setNo)).get()
+                .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                    @Override
+                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+
+                        Map<String, QueryDocumentSnapshot> docList = new ArrayMap<>();
+
+                        for (QueryDocumentSnapshot doc : queryDocumentSnapshots) {
+                            docList.put(doc.getId(), doc);
+                        }
+
+                        QueryDocumentSnapshot quesListDoc = docList.get("QUESTIONS_LIST");
+
+                        String count = quesListDoc.getString("COUNT");
+
+                        for (int i = 0; i < Integer.valueOf(count); i++) {
+                            String quesID = quesListDoc.getString("Q" + String.valueOf(i + 1) + "_ID");
+
+                            QueryDocumentSnapshot quesDoc = docList.get(quesID);
+
+                            questionList.add(new Question(
+                                    quesDoc.getString("QUESTION"),
+                                    quesDoc.getString("A"),
+                                    quesDoc.getString("B"),
+                                    quesDoc.getString("C"),
+                                    quesDoc.getString("D"),
+                                    Integer.valueOf(quesDoc.getString("ANSWER"))
+                            ));
+
+                        }
+
+                        setQuestion();
+
+                        loadingDialog.dismiss();
+
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(QuestionActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                        loadingDialog.dismiss();
+                    }
+                });
+
+    }
+
+    private void setQuestion() {
+        timer.setText(String.valueOf(10));
+
+        question.setText(questionList.get(0).getQuestion());
+        option1.setText(questionList.get(0).getOptionA());
+        option2.setText(questionList.get(0).getOptionB());
+        option3.setText(questionList.get(0).getOptionC());
+        option4.setText(questionList.get(0).getOptionD());
+
+
+        qCount.setText(String.valueOf(1) + "/" + String.valueOf(questionList.size()));
+
+        startTimer();
+
+        quesNum = 0;
+
+    }
+
+    private void startTimer() {
+        countDown = new CountDownTimer(12000, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                if (millisUntilFinished < 10000)
+                    timer.setText(String.valueOf(millisUntilFinished / 1000));
+            }
+
+            @Override
+            public void onFinish() {
+                changeQuestion();
+            }
+        };
+
+        countDown.start();
+
+    }
+
+
+    @Override
+    public void onClick(View v) {
+
+        int selectedOption = 0;
+
+        switch (v.getId()) {
+            case R.id.option1:
+                selectedOption = 1;
+                break;
+
+            case R.id.option2:
+                selectedOption = 2;
+                break;
+
+            case R.id.option3:
+                selectedOption = 3;
+                break;
+
+            case R.id.option4:
+                selectedOption = 4;
+                break;
+
+            default:
+        }
+
+        countDown.cancel();
+        checkAnswer(selectedOption, v);
+
+    }
+
+    private void checkAnswer(int selectedOption, View view) {
+
+        if (selectedOption == questionList.get(quesNum).getCorrectAns()) {
+            //Right Answer
+            ((Button) view).setBackgroundTintList(ColorStateList.valueOf(Color.GREEN));
+            score++;
+
+        } else {
+            //Wrong Answer
+            ((Button) view).setBackgroundTintList(ColorStateList.valueOf(Color.RED));
+
+            switch (questionList.get(quesNum).getCorrectAns()) {
+                case 1:
+                    option1.setBackgroundTintList(ColorStateList.valueOf(Color.GREEN));
+                    break;
+                case 2:
+                    option2.setBackgroundTintList(ColorStateList.valueOf(Color.GREEN));
+                    break;
+                case 3:
+                    option3.setBackgroundTintList(ColorStateList.valueOf(Color.GREEN));
+                    break;
+                case 4:
+                    option4.setBackgroundTintList(ColorStateList.valueOf(Color.GREEN));
+                    break;
+
+            }
+
+        }
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                changeQuestion();
+            }
+        }, 2000);
+
+
+    }
+
+    private void changeQuestion() {
+        if (quesNum < questionList.size() - 1) {
+            quesNum++;
+
+            playAnim(question, 0, 0);
+            playAnim(option1, 0, 1);
+            playAnim(option2, 0, 2);
+            playAnim(option3, 0, 3);
+            playAnim(option4, 0, 4);
+
+            qCount.setText(String.valueOf(quesNum + 1) + "/" + String.valueOf(questionList.size()));
+            timer.setText(String.valueOf(10));
+            startTimer();
+
+        } else {
+            // Go to Score Activity
+            Intent intent = new Intent(QuestionActivity.this, ScoreActivity.class);
+            intent.putExtra("SCORE", String.valueOf(score) + "/" + String.valueOf(questionList.size()));
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+            //QuestionActivity.this.finish();
+        }
+
+
+    }
+
+
+    private void playAnim(final View view, final int value, final int viewNum) {
+
+        view.animate().alpha(value).scaleX(value).scaleY(value).setDuration(500)
+                .setStartDelay(100).setInterpolator(new DecelerateInterpolator())
+                .setListener(new Animator.AnimatorListener() {
+                    @Override
+                    public void onAnimationStart(Animator animation) {
+
+                    }
+
+                    @Override
+                    public void onAnimationEnd(Animator animation) {
+                        if (value == 0) {
+                            switch (viewNum) {
+                                case 0:
+                                    ((TextView) view).setText(questionList.get(quesNum).getQuestion());
+                                    break;
+                                case 1:
+                                    ((Button) view).setText(questionList.get(quesNum).getOptionA());
+                                    break;
+                                case 2:
+                                    ((Button) view).setText(questionList.get(quesNum).getOptionB());
+                                    break;
+                                case 3:
+                                    ((Button) view).setText(questionList.get(quesNum).getOptionC());
+                                    break;
+                                case 4:
+                                    ((Button) view).setText(questionList.get(quesNum).getOptionD());
+                                    break;
+
+                            }
+
+
+                            if (viewNum != 0)
+                                ((Button) view).setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#E99C03")));
+
+
+                            playAnim(view, 1, viewNum);
+
+                        }
+
+                    }
+
+                    @Override
+                    public void onAnimationCancel(Animator animation) {
+
+                    }
+
+                    @Override
+                    public void onAnimationRepeat(Animator animation) {
+
+                    }
+                });
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+
+        countDown.cancel();
+    }
+
+
+    @Override
+    protected void onResume() {
+        // Register the accelerometer sensor
+        sensorManager.registerListener(shakeListener, sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+                SensorManager.SENSOR_DELAY_FASTEST);
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        /**
+         * Cancel register
+         */
+        sensorManager.unregisterListener(shakeListener);
+        super.onPause();
+    }
+
+    private class ShakeSensorListener implements SensorEventListener {
+
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+            // Avoid shaking all the time
+            if (isShake) {
+                return;
+            }
+            float[] values = event.values;
+            /*
+             * x : Acceleration of gravity in the x-axis direction, positive to the right
+             * y : Acceleration of gravity in the y-axis direction, forward is positive
+             * z : Acceleration of gravity in the z-axis direction, up is positive
+             */
+            float x = Math.abs(values[0]);
+            float y = Math.abs(values[1]);
+            float z = Math.abs(values[2]);
+            // When acceleration exceeds 25, shake successfully
+            if (x > 25 || y > 25 || z > 25) {
+                isShake = true;
+                // Play sound
+                playSound(QuestionActivity.this);
+                // Vibrate, pay attention to permissions in AndroidManifest.xml
+                vibrate(500);
+                // Imitate network delay operation, here you can go to the server...
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        // Cancel the countdown of the previous question
+                        countDown.cancel();
+                        // When shake, change to the next question.
+                        changeQuestion();
+                        isShake = false;
+                    }
+                }, 1000);
+            }
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        }
+    }
+
+    private void playSound(Context context) {
+        MediaPlayer player = MediaPlayer.create(context, R.raw.shake_sound);
+        player.start();
+    }
+
+    private void vibrate(long milliseconds) {
+        Vibrator vibrator = (Vibrator) getSystemService(Service.VIBRATOR_SERVICE);
+        vibrator.vibrate(milliseconds);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
